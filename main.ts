d69bf46a4f9085e2b140fa297ca7bfabb4d8e6bc
Raik Andritschke <58722460+CalliTGS3@users.noreply.github.com>
2023-02-28 10:45:20 +0100

    Variablen umbenannt

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     Fahren = false
-    basic.setLedColor(0xff0000)
+    basic.turnRgbLedOff()
 })
 let Richtung = 0
 let Geschwindigkeit = 0
@@ -13,24 +13,27 @@ let LedX = 0
 let NeigungY = 0
 let NeigungX = 0
 let Fahren = false
-let Empfindlichkeit = 30
+let MaxNeigungswinkel = 30
 radio.setGroup(1)
+basic.turnRgbLedOff()
 basic.forever(function () {
     if (Fahren) {
         NeigungX = input.rotation(Rotation.Roll)
         NeigungY = input.rotation(Rotation.Pitch)
-        NeigungX = Math.constrain(NeigungX, Empfindlichkeit * -1, Empfindlichkeit)
-        NeigungY = Math.constrain(NeigungY, Empfindlichkeit * -1, Empfindlichkeit)
+        NeigungX = Math.constrain(NeigungX, MaxNeigungswinkel * -1, MaxNeigungswinkel)
+        NeigungY = Math.constrain(NeigungY, MaxNeigungswinkel * -1, MaxNeigungswinkel)
         led.unplot(LedX, LedY)
-        LedX = Math.map(NeigungX, Empfindlichkeit * -1, Empfindlichkeit, 0, 4)
-        LedY = Math.map(NeigungY, Empfindlichkeit * -1, Empfindlichkeit, 0, 4)
+        LedX = Math.map(NeigungX, MaxNeigungswinkel * -1, MaxNeigungswinkel, 0, 4)
+        LedY = Math.map(NeigungY, MaxNeigungswinkel * -1, MaxNeigungswinkel, 0, 4)
         led.plot(LedX, LedY)
-        Geschwindigkeit = 100 - Math.map(NeigungY, Empfindlichkeit * -1, Empfindlichkeit, 0, 100)
-        Richtung = Math.map(NeigungX, Empfindlichkeit * -1, Empfindlichkeit, 0, 100)
+        Geschwindigkeit = 100 - Math.map(NeigungY, MaxNeigungswinkel * -1, MaxNeigungswinkel, 0, 100)
+        Richtung = Math.map(NeigungX, MaxNeigungswinkel * -1, MaxNeigungswinkel, 0, 100)
+        radio.sendValue("F", 1)
         radio.sendValue("G", Geschwindigkeit)
         radio.sendValue("R", Richtung)
         basic.pause(50)
     } else {
+        radio.sendValue("F", 0)
         basic.clearScreen()
     }
 })
